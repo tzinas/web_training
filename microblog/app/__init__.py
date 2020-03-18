ac1bf61d5f394bf8e78b2b173e566cf946604572
tzinas <atzinas2001@gmail.com>
2020-03-18 17:10:56 +0200

    Adds Followers

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,12 +1,13 @@
+import os
+import logging
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
-import os
 
 
 app = Flask(__name__)

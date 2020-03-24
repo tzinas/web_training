a9087472f482b9da2671302d20b4a5eb2cc4b439
tzinas <atzinas2001@gmail.com>
2020-03-24 21:14:09 +0200

    Async Email

--- microblog/app/email.py
+++ microblog/app/email.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask_mail import Message
+from threading import Thread
 
 from app import app, mail
 
@@ -8,7 +9,8 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
+
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
@@ -19,3 +21,8 @@ def send_password_reset_email(user):
                                         user=user, token=token),
                 html_body=render_template('email/reset_password.html',
                                         user=user, token=token))
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)

93735997fadbaf03cd81722e142dfbbb833bc1e8
tzinas <atzinas2001@gmail.com>
2020-04-16 15:47:25 +0300

    Translate

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-from flask import Flask
+from flask import Flask, request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,6 +10,8 @@ from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -20,6 +22,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 
 if not app.debug:
@@ -47,5 +51,9 @@ if not app.debug:
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
+
+@babel.localeselector
+def get_locale():
+    return 'en'
 
 from app import routes, models, errors
--- microblog/app/email.py
+++ microblog/app/email.py
@@ -4,6 +4,9 @@ from threading import Thread
 
 from app import app, mail
 
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
@@ -21,8 +24,3 @@ def send_password_reset_email(user):
                                         user=user, token=token),
                 html_body=render_template('email/reset_password.html',
                                         user=user, token=token))
-
-
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
